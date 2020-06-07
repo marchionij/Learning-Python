85b1d409e972736dc358211d19f6c55b7245398f
Jeffrey Marchioni <JeffreyMarchioni@Jeffreys-MBP.fios-router.home>
2020-06-07 17:43:56 -0400

    Python Basics

--- Ch2/variables_start.py
+++ Ch2/variables_start.py
@@ -3,13 +3,32 @@
 #
 
 # Declare a variable and initialize it
+f=0
+print(f)
 
 
 # # re-declaring the variable works
+f="abc"
+print(f)
 
 
 # # ERROR: variables of different types cannot be combined
+print("this is a string " + str(123))
+
 
 
 # Global vs. local variables in functions
+def someFunction():
+    # global f 
+    # Use if you want to make f a global variable
+    # AKA the function will change the value of f EVERYWHERE
+    f = "def"
+    print(f)
 
+someFunction()
+print(f)
+
+# del f
+# print (f)
+# Use del to delete the value of a variable
+# In this case, it will cause an error when printing f because f has no definition
