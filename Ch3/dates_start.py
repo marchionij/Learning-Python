faa3517d5a67deb0762c57ff3b9588638418ccec
Jeffrey Marchioni <JeffreyMarchioni@Jeffreys-MBP.fios-router.home>
2020-06-12 15:29:10 -0400

    Working with Dates and Times

--- Ch3/dates_start.py
+++ Ch3/dates_start.py
@@ -1,26 +1,36 @@
 #
 # Example file for working with date information
 #
-
+from datetime import date
+from datetime import time
+from datetime import datetime
 
 def main():
   ## DATE OBJECTS
   # Get today's date from the simple today() method from the date class
-
+  today = date.today()
+  print("Today's date is", today)
+  print('')
 
   # print out the date's individual components
+  print("Date Components:", today.day, today.month, today.year)
+  print('')
 
-  
   # retrieve today's weekday (0=Monday, 6=Sunday)
+  print("Today's weekday number is", today.weekday())
+  days = ["Mon", "Tue", "Wed", "Thu", "Fri", "Sat", "Sun"]
+  print("Which is a", days[today.weekday()])
+  print('')
 
-  
   ## DATETIME OBJECTS
   # Get today's date from the datetime class
-
+  today = datetime.now()
+  print("The current date and time is:",today)
+  print('')
 
   # Get the current time
-
-
+  t = datetime.time(datetime.now())
+  print("The current time is:", t)
   
 if __name__ == "__main__":
   main();
--- Ch3/formatting_start.py
+++ Ch3/formatting_start.py
@@ -7,20 +7,38 @@ from datetime import datetime
 def main():
   # Times and dates can be formatted using a set of predefined string
   # control codes 
+  now = datetime.now()
 
   #### Date Formatting ####
   
   # %y/%Y - Year, %a/%A - weekday, %b/%B - month, %d - day of month
+  # Lowercase stands for abbreviated years, weekdays, and months
+  print('')
+  print("Here is an example of the strftime formatting in use - 1 variable at a time.")
+  print(now.strftime("The current year is: %Y"))
+  print(now.strftime("The current weekday is: %A"))
+  print(now.strftime("The current month is: %B"))
+  print(now.strftime("The current day of the month is: %d"))
+  print('')
 
+  print("Here is an example of the strftime formatting in use - multiple variables at a time.")
+  print(now.strftime("%a, %d %B, %y"))
+  print('')
 
   # %c - locale's date and time, %x - locale's date, %X - locale's time
-
+  print("Here is an example of the gathering locale date and time.")
+  print(now.strftime("Locale date and time: %c"))
+  print(now.strftime("Locale date: %x"))
+  print(now.strftime("Locale time: %X"))
+  print('')
 
   #### Time Formatting ####
-  
+
+
   # %I/%H - 12/24 Hour, %M - minute, %S - second, %p - locale's AM/PM
-
-
+  print("Here is an example of gathering time components.")
+  print(now.strftime("Current time: %I:%M:%S, %p"))
+  print(now.strftime("24-hour time: %H:%M:%S, %p"))
 
 if __name__ == "__main__":
   main();
